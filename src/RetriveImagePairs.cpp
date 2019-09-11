443953bd640cdbb3dcd0343f9022e06e3d3dd98f
Outofnothing <chriszhou0628@gmail.com>
2019-09-11 12:29:15 +0800

    add opencv calibration

---
+++ src/RetriveImagePairs.cpp
@@ -0,0 +1,263 @@
+#include "Spinnaker.h"
+#include "SpinGenApi/SpinnakerGenApi.h"
+#include "Acquisition.hpp"
+#include "DataProcess.hpp"
+#include <iostream>
+#include <sstream>
+#include <chrono>
+#include <cstring>
+#include <Windows.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+//#define TRANS_FRAME
+
+using namespace Spinnaker;
+using namespace Spinnaker::GenApi;
+using namespace Spinnaker::GenICam;
+using namespace std;
+
+const int NUM_CAMERAS = 4;
+// Example entry point; please see Enumeration example for more in-depth 
+// comments on preparing and cleaning up the system.
+int main(int /*argc*/, char** /*argv*/)
+{   
+   
+	bool status = true;
+	DataProcess dataProcess;
+    // let the program know which camera to acquire image from
+    
+    cv::Mat image_LU, image_RU, image_RL, image_LL; // Left_Upper, Right_Upper, Right_Lower, Left_Lower
+	cv::WindowFlags window_type = cv::WINDOW_FULLSCREEN;
+	cv::namedWindow("CONCAT", window_type);
+	cv::namedWindow("map", window_type);
+    // Retrieve singleton reference to system object
+    SystemPtr system = System::GetInstance();
+    // Print Spinnaker library version
+    const LibraryVersion spinnakerLibraryVersion = system->GetLibraryVersion();
+    std::cout << "Spinnaker library version: "
+        << spinnakerLibraryVersion.major << "."
+        << spinnakerLibraryVersion.minor << "."
+        << spinnakerLibraryVersion.type << "."
+        << spinnakerLibraryVersion.build << "\n" << std::endl;
+
+    // Retrieve list of cameras from the system
+    CameraList camList = system->GetCameras();
+  
+    unsigned int numCameras = camList.GetSize();
+	int CameraIndex[4] = { 0,1,2,3 };
+	std::cout << "Number of cameras detected: " << numCameras << "\n" << std::endl;
+	// set current process as high priority (second highest, the highest is Real time)
+	SetPriorityClass(GetCurrentProcess(), HIGH_PRIORITY_CLASS);
+    CameraPtr pCam = nullptr;
+    try 
+    {
+        // Give each camera index
+        for (unsigned int i = 0; i < numCameras; i++)
+        {
+
+            // Select camera
+            pCam = camList.GetByIndex(i);
+            gcstring deviceSerialNumber;
+            if (pCam->TLDevice.DeviceSerialNumber != NULL && pCam->TLDevice.DeviceSerialNumber.GetAccessMode() == RO)
+            {
+                deviceSerialNumber =  pCam->TLDevice.DeviceSerialNumber.ToString();
+            }
+            else
+            {
+                cout << "Error: DeviceSerialNumber unavailable" << endl;
+            }
+            if (deviceSerialNumber=="18308397")
+            {
+                CameraIndex[i] = 0; // Left Upper
+            }
+            else if (deviceSerialNumber=="18308395")
+            {
+                CameraIndex[i] = 1; // Left Lower
+            }
+            else if (deviceSerialNumber == "18308399")
+            {
+                CameraIndex[i] = 2; // Right Upper               
+            }
+            else if (deviceSerialNumber == "18308393")
+            {
+                CameraIndex[i] = 3; // Right Lower
+            }
+			cout << "Camera Index on " << i << " is " << CameraIndex[i] << endl;
+        }
+		for (unsigned int i = 0; i < numCameras; i++)
+		{
+			pCam = camList.GetByIndex(i);
+			std::cout << endl << "Configuring Camera " << i << "..." << endl;
+			pCam->Init();
+			if (pCam->IsInitialized())
+			{
+				std::cout << "Camera " << i << " initialized successly" << endl;
+			}
+			// Config camera
+			status = status && ConfigCamera(pCam, CameraIndex[i]);
+			pCam->BeginAcquisition();
+			if (status)
+			{
+				std::cout << "Camera " << i << " config complete..." << "\n" << endl;
+			}
+			else
+			{
+				std::cout << "Camera " << i << " config failed..." << "\n" << endl;
+				// Clear camera list before releasing system
+				camList.Clear();
+				// Release system
+				system->ReleaseInstance();
+				std::cout << "Press Any Key to exit..." << endl;
+				getchar();
+				return -1;
+			}
+		}
+		// Create an array of CameraPtrs. This array maintenances smart pointer's reference
+		// count when CameraPtr is passed into grab thread as void pointer
+		AcquisitionParameters* paraList = new AcquisitionParameters[NUM_CAMERAS];
+		cv::Mat images[NUM_CAMERAS];
+		HANDLE* grabThreads = new HANDLE[NUM_CAMERAS];
+		for (int j = 0; j < NUM_CAMERAS; j++)
+		{
+			paraList[j].pCam = camList.GetByIndex(j);
+			paraList[j].cvImage = &images[CameraIndex[j]];
+		}
+        // acquire images and do something
+        // main part of this program
+        //cv::setMouseCallback("Left_Upper", tracker.Mouse_getColor, 0); 
+		bool first_time = true;
+		int num_Acquisition = 0; // init tracker after some images to assure auto balance finished
+		try
+		{
+			while (status)
+			{
+				// acquire images
+				for (unsigned int i = 0; i < NUM_CAMERAS; i++)
+				{
+					// Start grab thread
+					grabThreads[i] = CreateThread(nullptr, 0, AcquireImages, &paraList[i], 0, nullptr);
+					assert(grabThreads[i] != nullptr);
+				}
+				// Wait for all threads to finish
+				WaitForMultipleObjects(NUM_CAMERAS,		// number of threads to wait for 
+					grabThreads,				// handles for threads to wait for
+					TRUE,					// wait for all of the threads
+					INFINITE				// wait forever
+				);
+				// Check thread return code for each camera
+				for (unsigned int i = 0; i < NUM_CAMERAS; i++)
+				{
+					DWORD exitcode;
+
+					BOOL rc = GetExitCodeThread(grabThreads[i], &exitcode);
+					if (!rc)
+					{
+						cout << "Handle error from GetExitCodeThread() returned for camera at index " << i << endl;
+					}
+					else if (!exitcode)
+					{
+						cout << "Grab thread for camera at index " << i << " exited with errors."
+							"Please check onscreen print outs for error details" << endl;
+					}
+				}
+
+				cv::Mat combine, combine1, combine2, combine3;
+				cv::hconcat(images[2], images[0], combine1);
+				cv::hconcat(images[3], images[1], combine2);
+				cv::vconcat(combine1, combine2, combine);
+				cv::resize(combine, combine, cv::Size(1024, 1024));
+				for (int i = 0; i < 32; i++)
+				{
+					for (int j = 0; j < 32; j++)
+					{
+						cv::line(combine, cv::Point(32 * i, 0), cv::Point(32 * i, 1024), cv::Scalar(0, 255, 0));
+					}
+				}
+				cv::imshow("CONCAT", combine);
+				dataProcess.mapImages(images);
+				cv::hconcat(images[2], images[0], combine1);
+				cv::hconcat(images[3], images[1], combine2);
+				cv::vconcat(combine1, combine2, combine3);
+				
+				cv::resize(combine3, combine3, cv::Size(1024, 1024));
+				for (int i = 0; i < 32; i++)
+				{
+					for (int j = 0; j < 32; j++)
+					{
+						cv::line(combine3, cv::Point(32 * i, 0), cv::Point(32 * i, 1024), cv::Scalar(0, 255, 0));
+					}
+				}
+				cv::imshow("map", combine3);
+				//cv::imwrite("map.jpg", combine3);
+				int key = cv::waitKey(1);
+				if (key == 27)
+				{
+					status = false;
+				}
+				if (key == 32) // space
+				{
+
+					string string0 = "LeftUpper/" + to_string(num_Acquisition) + ".png";
+					string string1 = "LeftLower/" + to_string(num_Acquisition) + ".png";
+					string string2 = "RightUpper/" + to_string(num_Acquisition) + ".png";
+					string string3 = "RightLower/" + to_string(num_Acquisition) + ".png";
+					cv::imwrite(string0, images[0]);
+					cv::imwrite(string1, images[1]);
+					cv::imwrite(string2, images[2]);
+					cv::imwrite(string3, images[3]);
+					std::cout << "Add image pairs " << num_Acquisition << std::endl;
+					num_Acquisition++;
+				}
+				
+			}
+		}
+		catch (Spinnaker::Exception& e)
+		{
+			std::cout << "Spinnaker Error: during tracking: \n" << e.what() << endl;
+		}
+		catch (cv::Exception& e)
+		{
+			std::cout << "OpenCV Error: during tracking: \n" << e.what() << endl;
+		}
+		// Clear CameraPtr array and close all handles
+		for (unsigned int i = 0; i < NUM_CAMERAS; i++)
+		{    
+			CloseHandle(grabThreads[i]);
+		}
+
+		// Delete array pointer
+		delete[] paraList;
+		delete[] grabThreads;
+        cv::destroyAllWindows();
+		pCam = nullptr;
+    }
+    // sometimes AcquireImages may throw cv::Exception or Spinnaker::Exception
+    // using try catch makes sure we EndAcquisition for each camera
+    catch(Spinnaker::Exception &e)
+    {
+        std::cout << "Spinnaker Error: during Running Camera\n" << e.what() << endl;
+    }
+    catch (cv::Exception& e)  
+    {      
+        std::cout << "OpenCV Error: during Running Camera: \n" << e.what() << endl;
+    }  
+    // EndAcquisition
+    for (unsigned int i = 0; i < NUM_CAMERAS; i++)
+    {
+        // Select camera
+        pCam = camList.GetByIndex(i);
+        pCam->EndAcquisition();
+		ResetExposure(pCam);
+		ResetTrigger(pCam);
+        pCam->DeInit();
+		pCam = nullptr; // 一定要设为nullptr, 否则在system->ReleaseInstance()时会出错
+    }
+    //pCam = NULL;
+
+    // Clear camera list before releasing system
+    camList.Clear();
+    system->ReleaseInstance();
+    
+    return true;
+}
---
+++ src/StereoCalib.cpp
@@ -0,0 +1,140 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+vector< vector< Point3f > > object_points;
+vector< vector< Point2f > > imagePoints1, imagePoints2;
+vector< Point2f > corners1, corners2;
+vector< vector< Point2f > > left_img_points, right_img_points;
+
+Mat img1, img2, gray1, gray2;
+cv::Size imageSize(2048, 2048);
+
+void load_image_points(int board_width, int board_height, int num_imgs, float square_size,
+	char* upperimg_dir, char* lowerimg_dir) {
+
+	Size board_size = Size(board_width, board_height);
+	int board_n = board_width * board_height;
+
+	for (int i = 1; i <= num_imgs; i++) {
+		char left_img[100], right_img[100];
+		sprintf(left_img, "%s%d.png", upperimg_dir, i);
+		sprintf(right_img, "%s%d.png", lowerimg_dir, i);
+		img1 = imread(left_img, cv::IMREAD_COLOR);
+		img2 = imread(right_img, cv::IMREAD_COLOR);
+		cvtColor(img1, gray1, cv::COLOR_BGR2GRAY);
+		cvtColor(img2, gray2, cv::COLOR_BGR2GRAY);
+
+		bool found1 = false, found2 = false;
+
+		found1 = cv::findChessboardCorners(img1, board_size, corners1,
+			cv::CALIB_CB_ADAPTIVE_THRESH | cv::CALIB_CB_FILTER_QUADS);
+		found2 = cv::findChessboardCorners(img2, board_size, corners2,
+			cv::CALIB_CB_ADAPTIVE_THRESH | cv::CALIB_CB_FILTER_QUADS);
+
+
+		if (!found1 || !found2) {
+			cout << "Chessboard find error!" << endl;
+			cout << "leftImg: " << left_img << " and rightImg: " << right_img << endl;
+			continue;
+		}
+
+		if (found1)
+		{
+			cv::cornerSubPix(gray1, corners1, cv::Size(5, 5), cv::Size(-1, -1),
+				cv::TermCriteria(TermCriteria::EPS | TermCriteria::MAX_ITER, 30, 0.1));
+			cv::drawChessboardCorners(gray1, board_size, corners1, found1);
+		}
+		if (found2)
+		{
+			cv::cornerSubPix(gray2, corners2, cv::Size(5, 5), cv::Size(-1, -1),
+				cv::TermCriteria(TermCriteria::EPS | TermCriteria::MAX_ITER, 30, 0.1));
+			cv::drawChessboardCorners(gray2, board_size, corners2, found2);
+		}
+
+		vector< Point3f > obj;
+		for (int i = 0; i < board_height; i++)
+			for (int j = 0; j < board_width; j++)
+				obj.push_back(Point3f((float)j * square_size, (float)i * square_size, 0));
+
+		if (found1 && found2) {
+			cout << i << ". Found corners!" << endl;
+			imagePoints1.push_back(corners1);
+			imagePoints2.push_back(corners2);
+			object_points.push_back(obj);
+		}
+	}
+	for (int i = 0; i < imagePoints1.size(); i++) {
+		vector< Point2f > v1, v2;
+		for (int j = 0; j < imagePoints1[i].size(); j++) {
+			v1.push_back(Point2f((double)imagePoints1[i][j].x, (double)imagePoints1[i][j].y));
+			v2.push_back(Point2f((double)imagePoints2[i][j].x, (double)imagePoints2[i][j].y));
+		}
+		left_img_points.push_back(v1);
+		right_img_points.push_back(v2);
+	}
+}
+
+int main(int argc, char const* argv[])
+{
+	char* leftcalib_file;
+	char* rightcalib_file;
+	char* out_file = "Left.yml";
+	int num_imgs = 10;
+
+	
+	int c;
+
+
+	load_image_points(9, 12, num_imgs, 45, "Left_Upper", "Left_Lower");
+
+	printf("Starting Calibration\n");
+	cv::Mat D1, D2, R, T, E, F;
+	cv::Mat M1 = cv::Mat::eye(3, 3, CV_64F);
+	cv::Mat M2 = cv::Mat::eye(3, 3, CV_64F);
+
+
+	stereoCalibrate(object_points, left_img_points, right_img_points, M1, D1, M2, D2, img1.size(), R, T, E, F,
+		cv::CALIB_FIX_ASPECT_RATIO | cv::CALIB_ZERO_TANGENT_DIST |
+		cv::CALIB_SAME_FOCAL_LENGTH,
+		cv::TermCriteria(cv::TermCriteria::COUNT | cv::TermCriteria::EPS, 100,
+			1e-5));
+
+	cv::FileStorage fs1(out_file, cv::FileStorage::WRITE);
+	fs1 << "K1" << M1;
+	fs1 << "K2" << M2;
+	fs1 << "D1" << D1;
+	fs1 << "D2" << D2;
+	fs1 << "R" << R;
+	fs1 << "T" << T;
+	fs1 << "E" << E;
+	fs1 << "F" << F;
+
+	printf("Done Calibration\n");
+
+	printf("Starting Rectification\n");
+
+	cv::Mat R1, R2, P1, P2, Q, map11, map12, map21, map22;
+
+	stereoRectify(M1, D1, M2, D2, img1.size(), R, T, R1, R2, P1, P2, Q);
+	// Precompute maps for cvRemap()
+	initUndistortRectifyMap(M1, D1, R1, P1, imageSize, CV_16SC2, map11,
+		map12);
+	initUndistortRectifyMap(M2, D2, R2, P2, imageSize, CV_16SC2, map21,
+		map22);
+	fs1 << "R1" << R1;
+	fs1 << "R2" << R2;
+	fs1 << "P1" << P1;
+	fs1 << "P2" << P2;
+	fs1 << "Q" << Q;
+
+	printf("Done Rectification\n");
+
+	return 0;
+}
